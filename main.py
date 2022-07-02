ff3388205071ed951dc6a8f4ca324e1313b066a5
krtolina <krtolinat@gmail.com>
2022-07-03 01:59:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ TRAIN_TEST_RATIO = 80  # PERCENT
 START_DATE = '2014-01-01'
 END_DATE = '2022-01-01'
 
+# GET DATA
 price_data = yf.download('BTC-USD', start=START_DATE, end=END_DATE)
 close_prices = price_data['Close'].values
 training_data_len = math.ceil(len(close_prices) * TRAIN_TEST_RATIO / 100)
 
+# SCALE DATA FROM 0 TO 1
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(close_prices.reshape(-1, 1))
 
+# MAKE SAMPLES FOR TRAINING
 train_data = scaled_data[0: training_data_len, :]
 x_train, y_train = [], []
 
@@ -29,16 +32,17 @@ for i in range(SAMPLE_SPAN, len(train_data)):
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
+# MAKE SAMPLES FOR TESTING
 test_data = scaled_data[training_data_len - SAMPLE_SPAN:, :]
-x_test = []
-y_test = close_prices[training_data_len:]
+x_test, y_test = [], close_prices[training_data_len:]
 
 for i in range(SAMPLE_SPAN, len(test_data)):
     x_test.append(test_data[i - SAMPLE_SPAN:i, 0])
-x_test = np.array(x_test)
 
+x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
+# MAKE AND TRAIN MODEL
 model = keras.Sequential()
 model.add(layers.CuDNNLSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(layers.CuDNNLSTM(100, return_sequences=False))
@@ -47,12 +51,14 @@ model.add(layers.Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, batch_size=1, epochs=EPOCHS)
 
+# GET PREDICTIONS FROM TEST INPUT
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
 rmse = np.sqrt(np.mean(predictions - y_test) ** 2)
 print("Root Mean Square Error:")
 print(rmse)
 
+# PLOT DATA
 data = price_data.filter(['Close'])
 train = data[:training_data_len]
 validation = data[training_data_len:]
